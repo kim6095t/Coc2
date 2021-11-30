732a29e51458388bbbb7ab17ec64e6cce38b5597
kimtaeyoon <kim6095t@naver.com>
2021-11-30 15:26:24 +0900

    11.30 cafe

--- Assets/Scripts/Manager/InTownUnitManager.cs
+++ Assets/Scripts/Manager/InTownUnitManager.cs
@@ -10,28 +10,32 @@ public class InTownUnitManager : Singletone<InTownUnitManager>
 
     float timer;
     bool useTimer;
-    Unit.Unit_TYPE lastType;
-    List<Unit.Unit_TYPE> unitList;
+    unitStruct lastUnit;
+    List<unitStruct> unitList;
 
 
     struct unitStruct
     {
-        Unit.Unit_TYPE;
-
+        public CheckUnitSlot slot;
+        public Unit.Unit_TYPE type;
+        public Text countText;
     }
 
     private void Start()
     {
-        unitList = new List<Unit.Unit_TYPE>();
+        unitList = new List<unitStruct>();
 
-        lastType = Unit.Unit_TYPE.None;
+        lastUnit.type = Unit.Unit_TYPE.None;
         timer = 0f;
         useTimer = false;
     }
 
     private void Update()
     {
-        if (!useTimer && unitList[0] != null)
+        if (unitList.Count <= 0)
+            return;
+
+        if (!useTimer)
         {
             timer = 0f;
             StartCoroutine(createTime(unitList[0]));
@@ -40,26 +44,45 @@ public class InTownUnitManager : Singletone<InTownUnitManager>
 
     public void OnCreatedUnit(Text unitCount, Text priceText, Unit.Unit_TYPE type)
     {
-        unitList.Add(type);
+        //리스트에 추가
+        unitStruct unit = new unitStruct();
+        unit.type = type;
+        unit.countText = unitCount;
 
-        CheckUnitSlot slot = Instantiate(checkUnit);
-        slot.transform.parent = trans;
+        if (lastUnit.type != type) {
+            CheckUnitSlot slot = Instantiate(checkUnit);
+            slot.transform.parent = trans;
+            slot.unitImage.sprite = Resources.Load<Sprite>($"UnitSprite/{type}");
+            slot.unitCount.text = "1";
+            unit.slot = slot;
+
+            lastUnit = unit;
+        }
+        else
+        {
+            int num = int.Parse(lastUnit.slot.unitCount.text);
+            lastUnit.slot.unitCount.text = (num+1).ToString();
+            unit.slot = lastUnit.slot;
+        }
+        
+        unitList.Add(unit);
+
         MyResourceData.Instance.UseJellyToMine(int.Parse(priceText.text));
-        lastType = type;
-
     }
 
-    IEnumerator createTime(Unit.Unit_TYPE type)
+    IEnumerator createTime(unitStruct unit)
     {
         useTimer = true;
         while (true)
         {
             timer += Time.deltaTime;
+            unit.slot.fillBox.fillAmount = timer / 3f;
 
             if (timer > 3f)
             {
-                UnitManager.Instance.unitData[(int)type].countUnit += 1;
-                unitCount.text = string.Format("{0:#,##0}", UnitManager.Instance.unitData[(int)type].countUnit);
+                UnitManager.Instance.unitData[(int)unit.type].countUnit += 1;
+                unit.countText.text = string.Format("{0:#,##0}", UnitManager.Instance.unitData[(int)unit.type].countUnit);
+                Destroy(unit.slot.gameObject);
 
                 unitList.RemoveAt(0);
                 useTimer = false;
@@ -67,6 +90,5 @@ public class InTownUnitManager : Singletone<InTownUnitManager>
             }
             yield return null;
         }
-        
     }
 }
